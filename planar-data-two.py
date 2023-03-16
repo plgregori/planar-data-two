b66a79aa4d4818174138dbc80d60195fdc1d1363
paolo.gregori <plgregori@gmail.com>
2023-03-16 16:26:10 +0100

    comments for linear

--- planar-data-two.py
+++ planar-data-two.py
@@ -28,54 +28,74 @@ plt.show()
 
 # Normally we would want to preprocess the dataset so that each feature has zero mean and unit standard deviation, but in this case the features are already in a nice range from -1 to 1, so we skip this step.
 
-#Train a Linear Classifier
+#We now train the linear classifier
 
-# initialize parameters randomly
+# We start by initializing the weights to random values and the bias to zero
+
 W = 0.01 * np.random.randn(D,K)
 b = np.zeros((1,K))
 
-# some hyperparameters
+# We introduce two hyperparameters. step_size is the usual step parameter for gradient descent. reg is the constant appearing in the regularization of the loss function
+
 step_size = 1e-0
 reg = 1e-3 # regularization strength
 
-# gradient descent loop
+# We now implement the gradient descent loop
+
 num_examples = X.shape[0]
 for i in range(200):
 
-  # evaluate class scores, [N x K]
+  # First we evaluate the scores
   scores = np.dot(X, W) + b
 
-  # compute the class probabilities
+  # Then the normalized exponential probabilities
   exp_scores = np.exp(scores)
   probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
 
-  # compute the loss: average cross-entropy loss and regularization
+  # Then the loss function in terms of logarithmic probabilities + regularization
   correct_logprobs = -np.log(probs[range(num_examples),y])
   data_loss = np.sum(correct_logprobs)/num_examples
   reg_loss = 0.5*reg*np.sum(W*W)
   loss = data_loss + reg_loss
+  # And we print the loss every 10 iterations
   if i % 10 == 0:
         print("iteration %d: loss %f" % (i, loss))
 
-  # compute the gradient on scores
+  # We compute the derivatives of the loss w.r.t. the scores
   dscores = probs
   dscores[range(num_examples),y] -= 1
   dscores /= num_examples
 
-  # backpropate the gradient to the parameters (W,b)
+  # And w.r.t. the parameters W and b
   dW = np.dot(X.T, dscores)
   db = np.sum(dscores, axis=0, keepdims=True)
 
-  dW += reg*W # regularization gradient
+  dW += reg*W # This is to include also the regularization contribution
 
-  # perform a parameter update
+  # We update the parameters
   W += -step_size * dW
   b += -step_size * db
 
-# evaluate training set accuracy
+# We evaluate the accuracy of our linear classifier.
+# First using argmax, we select for each row the position of the highest value. e.g.: np.argmax([[0.3, 2.2, 4.5]]) = 2
+# Then we use predicted_class == y to create an array of ones and zeros depending on whether the elements of predicted_class and y match. Note that this works only if we defined them as numpy arrays
+# Finally we use np.mean to sum over all the 'one' entries and divide by the total number of entries
+
 scores = np.dot(X, W) + b
 predicted_class = np.argmax(scores, axis=1)
+print(predicted_class)
+print(y)
 print('training accuracy: %.2f' % (np.mean(predicted_class == y)))
+
+# Finally, we print the decision boundaries associated with our prediction
+# First, we introduce minimum and maximum values for the two coordinates    
+# With np.arange we create two arrays of x and y coordinates, evenly spaced by h and going from the minimum to the maximum values
+# With np.meshgrid we combine the two arrays into two matrices of coordinates xx and yy, which define a grid
+# With .ravel() flattens xx and yy into arrays, and with c_ we stack them and transpose them to create something with the shape of a dataset
+# We then multiply this with W and sum b to obtain a prediction on the new dataset
+# With np.argmax we select the strongest class, and reshape it in such a way that it looks like a coordinate matrix like xx or yy
+# plt.contourf takes as arguments two coordinate matrices, which generate a grid, and a color matrix, which assigns to each site in the grid a color
+# By setting alpha=0.8 we make the prediction slightly less transparent than the training dots
 
 h = 0.02
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
